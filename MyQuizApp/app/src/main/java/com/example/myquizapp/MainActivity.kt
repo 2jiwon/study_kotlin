2518306a52ceed5f938999c4823bc94b49337dcb
이지원 <ljwjulian@gmail.com>
2023-11-20 14:25:16 +0900

    퀴즈앱 - Quizquestions 액티비티 생성하고 레이아웃 작성 + 메인에서 start 버튼 누르면 이동하도록 셋팅

--- MyQuizApp/app/src/main/java/com/example/myquizapp/MainActivity.kt
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById<Button>(R.id.btn_start)
+        val etName : EditText = findViewById(R.id.et_name)
+
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()) {
+                Toast.makeText(this, "Please enter your name.", Toast.LENGTH_LONG).show()
+            } else {
+                // 여기에서 다른 액티비로 넘어가는 처리
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+
+                startActivity(intent)
+                // finish()를 추가하면 이동한 액티비티에서 뒤로가기 버튼을 눌렀을 때 앱을 종료하게 됨
+                finish()
+            }
+        }
     }
 }
