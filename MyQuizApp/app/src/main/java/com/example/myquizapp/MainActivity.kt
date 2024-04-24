6a002af43ad497f8b7cef87c0083c5de93a913da
이지원 <ljwjulian@gmail.com>
2024-04-24 17:17:00 +0900

    퀴즈앱 - 사용자 이름을 입력받아 질문 액티비티로 넘기는 부분 추가

--- MyQuizApp/app/src/main/java/com/example/myquizapp/MainActivity.kt
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -19,8 +19,12 @@ class MainActivity : AppCompatActivity() {
             if (etName.text.isEmpty()) {
                 Toast.makeText(this, "Please enter your name.", Toast.LENGTH_LONG).show()
             } else {
-                // 여기에서 다른 액티비로 넘어가는 처리
+                // 여기에서 다른 액티비티로 넘어가는 처리
                 val intent = Intent(this, QuizQuestionsActivity::class.java)
+
+                // 액티비티를 시작하는 동시에 추가로 정보를 보내기 위해 putExtra 사용
+                // intent로 한 액티비티에서 다른 액티비티로 추가 정보를 넘긴 다음, 다른 액티비티에서 정보값을 회수할 수 있다.
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
 
                 startActivity(intent)
                 // finish()를 추가하면 이동한 액티비티에서 뒤로가기 버튼을 눌렀을 때 앱을 종료하게 됨
