6dd63bfa31536ca7494f6ddebbd5883b98500304
이지원 <ljwjulian@gmail.com>
2024-05-30 09:17:11 +0900

    NewQuizApp - Helper class에서 TextToSpeech 다시 시도

--- NewQuizApp/app/src/main/java/com/example/newquizapp/Helper.kt
+++ NewQuizApp/app/src/main/java/com/example/newquizapp/Helper.kt
@@ -1,7 +1,10 @@
 package com.example.newquizapp
 
 import android.content.Context
+import android.os.Build
+import android.speech.tts.TextToSpeech
 import android.widget.Toast
+import java.util.*
 
 class Helper {
     var toast: Toast? = null
@@ -11,6 +14,32 @@ class Helper {
         var toastLength = if (long) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
         toast = Toast.makeText(context, message, toastLength)
         toast!!.show()
+    }
 
+    private var tts: TextToSpeech? = null
+
+    fun initTextToSpeech(context: Context) {
+        // 롤리팝(API 21, Android 5.0)이상에서만 지원됨
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Toast.makeText(context, "지원하지 않는 SDK 버전입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        tts = TextToSpeech(context) {
+            if (it == TextToSpeech.SUCCESS) {
+                val result = tts?.setLanguage(Locale.KOREAN)
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(context, "지원하지 않는 언어입니다.", Toast.LENGTH_SHORT).show()
+//                    return@TextToSpeech
+                }
+                Toast.makeText(context, "TTS 설정 성공", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "TTS 설정 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun ttsSpeak(str: String) {
+        tts?.speak(str, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 }
