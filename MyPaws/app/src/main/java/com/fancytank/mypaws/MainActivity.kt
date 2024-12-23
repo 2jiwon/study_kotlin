834a35027d8b25ca741f2f2ec6d12643641727a0
이지원 <ljwjulian@gmail.com>
2024-12-23 18:29:52 +0900

    viewBinding 사용해서 textview를 감추고,새로운 text가 나타나게 하도록 수정

--- MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
+++ MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.fancytank.mypaws
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.fancytank.mypaws.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +16,20 @@ class MainActivity : AppCompatActivity() {
 
         installSplashScreen()
 
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            if (binding.etUsername.text.toString().isEmpty()) {
+                Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                // 반가워요! 부분을 안보이게 하고
+                binding.tvWelcome.visibility = View.INVISIBLE
+                // 새로운 질문을 넣는다
+                binding.tvQUsername.text = "세상에서 가장 이쁜 내새꾸의 이름은 무엇인가요?"
+
+            }
+        }
+
     }
 }
