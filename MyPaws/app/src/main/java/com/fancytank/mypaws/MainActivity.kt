b898e8551ff6a4da5e6597060641579ab7dab174
이지원 <ljwjulian@gmail.com>
2025-01-06 11:54:12 +0900

    버튼,텍스트input의 이름을 바꾸고 목록으로 관리하도록 수정

--- MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
+++ MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
@@ -11,25 +11,54 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fancytank.mypaws.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val answers = mutableListOf<String>() // 입력된 답변 저장 리스트
+    private val questions = listOf( // 질문 목록
+        "세상에서 가장 이쁜 내새꾸의 이름은 무엇인가요?",
+        "내새꾸가 좋아하는 음식은 무엇인가요?",
+        "내새꾸가 가장 좋아하는 장난감은 무엇인가요?"
+    )
+    private var currentQuestionIndex = 0 // 현재 질문의 인덱스
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         installSplashScreen()
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnStart.setOnClickListener {
-            if (binding.etUsername.text.toString().isEmpty()) {
+        // 첫 번째 질문 설정
+        binding.tvQuestions.text = questions[currentQuestionIndex]
+
+        binding.btnNext.setOnClickListener {
+            val input = binding.etAnswers.text.toString()
+
+            if (input.isEmpty()) {
                 Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else {
                 // 반가워요! 부분을 안보이게 하고
                 binding.tvWelcome.visibility = View.INVISIBLE
-                // 새로운 질문을 넣는다
-                binding.tvQUsername.text = "세상에서 가장 이쁜 내새꾸의 이름은 무엇인가요?"
+                // 입력된 데이터 저장
+                answers.add(input)
+                // 입력란 초기화
+                binding.etAnswers.text!!.clear()
 
+                // 다음 질문으로 이동
+                if (currentQuestionIndex < questions.size -1) {
+                    currentQuestionIndex++
+                    binding.tvQuestions.text = questions[currentQuestionIndex]
+                } else {
+                    // 질문이 끝났을 경우
+                    binding.tvQuestions.text = "감사합니다."
+                    binding.etAnswers.visibility = View.GONE
+                    binding.btnNext.visibility = View.GONE
+                    binding.tvWelcome.visibility = View.VISIBLE
+                    binding.tvWelcome.text = "답변: ${answers.joinToString(",")}"
+                }
             }
         }
-
     }
 }
