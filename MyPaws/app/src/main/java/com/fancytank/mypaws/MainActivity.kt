b5ab6655b0747c8b988a26db8d45bee22605f6b0
이지원 <ljwjulian@gmail.com>
2025-02-11 11:45:47 +0900

    MyPaws - 정보가 이미 있을 때와 없을 때 실행할 액티비티 구분

--- MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
+++ MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,8 +15,11 @@ import com.fancytank.mypaws.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MAIN ACTIVITY :: "
 
     private lateinit var binding: ActivityMainBinding
     private val base_questions = listOf( // 질문 목록
@@ -26,6 +30,10 @@ class MainActivity : AppCompatActivity() {
         "내새꾸에게 불리고 싶은 이름을 입력하세요.",
         "내새꾸의 이름이 뭔가요?"
     )
+
+    var user : User? = null
+    var pet : List<Pet>? = null
+
     private var currentQuestionIndex = 0 // 현재 질문의 인덱스
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,98 +42,157 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 첫 번째 질문 설정
-        binding.tvQuestions.text = base_questions[currentQuestionIndex]
-        // 힌트
-        binding.tilName.hint = base_hints[currentQuestionIndex]
+        // LoginActivity에서 전달받은 정보 가져오기
+        val googleId = intent.getStringExtra("GOOGLE_ID")
 
-        // 처음에 "다음" 버튼 숨김
-        binding.btnNext.visibility = View.GONE
+        googleId?.let { Log.d(TAG, it) }
 
-        binding.etAnswers.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // 텍스트가 변경될 때 호출
-                binding.btnNext.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-//                TODO("Not yet implemented")
-            }
-        })
-
-        // 사용자 정보를 먼저 저장
-        val userInfo = User(email = "test@example.com",
-            password = "1234",
-            username = "testUser", nickname = "")
+        // 기존에 이미 사용자 정보가 있는지 확인
         GlobalScope.launch(Dispatchers.IO) {
             val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
-            userDao.insertUser(userInfo)
-        }
+            googleId?.let { user = userDao.getUserByEmail(it) }
 
-        binding.btnNext.setOnClickListener {
-            val input = binding.etAnswers.text.toString()
+            Log.d(TAG, "user :: $user")
 
-            if (input.isEmpty()) {
-                Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
-            } else {
-                // 반가워요! 부분을 안보이게 하고
-                binding.tvWelcome.visibility = View.INVISIBLE
-                // 입력된 데이터 저장
-                Constants.answers.add(input)
+            if (user != null) {
+                val petDao = AppDatabase.getInstance(this@MainActivity).petDao()
+                user?.id?.let { pet = petDao.getPetsByUserId(it) }
 
-                // 테이블에 저장
-                if (currentQuestionIndex == 0) {
-                    userInfo.nickname = input
+                Log.d(TAG, user.toString())
 
-                    GlobalScope.launch(Dispatchers.IO) {
-                        val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
-                        userDao.updateUser(userInfo)
-                    }
-                } else {
+                // 만약 둘 다 이미 정보가 있으면 바로 ChatActivity로 넘어감
+                withContext(Dispatchers.Main) {
+                    user?.let { u ->
+                        pet?.let { p ->
+                            val prompt = OpenAIClient().generatePrompt(u, p[0])
 
-                    GlobalScope.launch(Dispatchers.IO) {
-                        val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
-                        val user = userDao.getUserByEmail(userInfo.email)
-
-                        val pet = Pet(
-                            name = input,
-                            userId = user?.id ?: 0L,
-                            type = "",
-                            breed = "",
-                            age = 0,
-                            bodyColor = "",
-                            eyeColor = ""
-                        )
-                        val petDao = AppDatabase.getInstance(this@MainActivity).petDao()
-                        petDao.insertPet(pet)
+                            OpenAIClient().generateResponse(
+                                prompt,
+                                onSuccess = { response ->
+                                    runOnUiThread {
+                                        // 결과를 화면에 표시
+                                        val intent = Intent(this@MainActivity, ChatActivity::class.java)
+                                        intent.putExtra("AI_RESPONSE", response)
+                                        startActivity(intent)
+                                    }
+                                },
+                                onError = { error ->
+                                    runOnUiThread {
+                                        Toast.makeText(this@MainActivity, error, Toast.LENGTH_LONG).show()
+                                        Log.d("Error :", error)
+                                    }
+                                }
+                            )
+                        }
                     }
                 }
 
-                // 입력란 초기화
-                binding.etAnswers.text!!.clear()
+            } else {
 
-                // 다음 질문으로 이동
-                if (currentQuestionIndex < base_questions.size -1) {
-                    currentQuestionIndex++
+                withContext(Dispatchers.Main) {
+                    // user가 null일 경우 아래 부분 실행
+                    val username: String = intent.getStringExtra("DISPLAY_NAME") ?: ""
+                    val profilePicture = intent.getStringExtra("PROFILE_PICTURE_URI") ?: ""
+
+                    Log.d(TAG, "username :: $username")
+
+                    // 사용자 정보를 먼저 저장
+                    val userInfo = User(
+                        email = googleId.toString(),
+                        password = "1234",
+                        username = username,
+                        nickname = "",
+                        pictureUrl = profilePicture
+                    )
+                    GlobalScope.launch(Dispatchers.IO) {
+                        val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
+                        userDao.insertUser(userInfo)
+                    }
+
+                    // 첫 번째 질문 설정
                     binding.tvQuestions.text = base_questions[currentQuestionIndex]
+                    // 힌트
                     binding.tilName.hint = base_hints[currentQuestionIndex]
-                } else {
-                    // 질문이 끝났을 경우
-//                    binding.tvQuestions.text = "감사합니다."
-//                    binding.etAnswers.visibility = View.GONE
-//                    binding.btnNext.visibility = View.GONE
-//                    binding.tvWelcome.visibility = View.VISIBLE
-//                    binding.tvWelcome.text = "답변: ${answers.joinToString(",")}"
 
-                    val intent = Intent(this, QuestionsActivity::class.java)
+                    // 처음에 "다음" 버튼 숨김
+                    binding.btnNext.visibility = View.GONE
 
-                    startActivity(intent)
+                    binding.etAnswers.addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                            //                TODO("Not yet implemented")
+                        }
 
-                    finish()
+                        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                            // 텍스트가 변경될 때 호출
+                            binding.btnNext.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                        }
+
+                        override fun afterTextChanged(s: Editable?) {
+                            //                TODO("Not yet implemented")
+                        }
+                    })
+
+                    binding.etAnswers.setText(username)
+
+                    binding.btnNext.setOnClickListener {
+                        val input = binding.etAnswers.text.toString()
+
+                        Log.d(TAG, "input :: $input")
+
+                        if (input.isEmpty()) {
+                            Toast.makeText(this@MainActivity, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                        } else {
+                            // 반가워요! 부분을 안보이게 하고
+                            binding.tvWelcome.visibility = View.INVISIBLE
+                            // 입력된 데이터 저장
+                            Constants.answers.add(input)
+
+                            // 테이블에 저장
+                            if (currentQuestionIndex == 0) {
+                                userInfo.nickname = input
+
+                                GlobalScope.launch(Dispatchers.IO) {
+                                    val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
+                                    userDao.updateUser(userInfo)
+                                }
+                            } else {
+
+                                GlobalScope.launch(Dispatchers.IO) {
+                                    val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
+                                    val user = userDao.getUserByEmail(userInfo.email)
+
+                                    val pet = Pet(
+                                        name = input,
+                                        userId = user?.id ?: 0L,
+                                        type = "",
+                                        breed = "",
+                                        age = 0,
+                                        bodyColor = "",
+                                        eyeColor = ""
+                                    )
+                                    val petDao = AppDatabase.getInstance(this@MainActivity).petDao()
+                                    petDao.insertPet(pet)
+                                }
+                            }
+
+                            // 입력란 초기화
+                            binding.etAnswers.text!!.clear()
+
+                            // 다음 질문으로 이동
+                            if (currentQuestionIndex < base_questions.size - 1) {
+                                currentQuestionIndex++
+                                binding.tvQuestions.text = base_questions[currentQuestionIndex]
+                                binding.tilName.hint = base_hints[currentQuestionIndex]
+                            } else {
+                                // 질문이 끝났을 경우
+                                val intent = Intent(this@MainActivity, QuestionsActivity::class.java)
+
+                                startActivity(intent)
+
+                                finish()
+                            }
+                        }
+                    }
                 }
             }
         }
