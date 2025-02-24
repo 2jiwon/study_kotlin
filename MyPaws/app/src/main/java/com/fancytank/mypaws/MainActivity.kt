36b5e49ce414497b243a3d308fef03baa48e5cd9
이지원 <ljwjulian@gmail.com>
2025-02-24 16:31:39 +0900

    MyPaws - 유저로그인정보만 있고 펫 정보가 없을 때 대비 추가(임시 복붙... 정리 필요)

--- MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
+++ MyPaws/app/src/main/java/com/fancytank/mypaws/MainActivity.kt
@@ -19,7 +19,7 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG = "MAIN ACTIVITY :: "
+    private val TAG = "MAIN ACTIVITY :: "
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var baseQuestions: List<String>
@@ -55,34 +55,130 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "user :: $user")
 
             if (user != null) {
-                val petDao = AppDatabase.getInstance(this@MainActivity).petDao()
-                user?.id?.let { pet = petDao.getPetsByUserId(it) }
+                user?.let { u ->
+                    val petDao = AppDatabase.getInstance(this@MainActivity).petDao()
+                    u.id.let { pet = petDao.getPetsByUserId(it) }
 
-                Log.d(TAG, user.toString())
+                    Log.d(TAG, user.toString())
+                    Log.d(TAG, pet.toString())
 
-                // 만약 둘 다 이미 정보가 있으면 바로 ChatActivity로 넘어감
-                withContext(Dispatchers.Main) {
-                    user?.let { u ->
-                        pet?.let { p ->
-                            val prompt = OpenAIClient().generatePrompt(u, p[0])
+                    if (pet.isNullOrEmpty()) {
+                        withContext(Dispatchers.Main) {
+                            // 숨겼던 UI 복원
+                            binding.root.visibility = View.VISIBLE
 
-                            OpenAIClient().generateResponse(
-                                prompt,
-                                onSuccess = { response ->
-                                    runOnUiThread {
-                                        // 결과를 화면에 표시
-                                        val intent = Intent(this@MainActivity, ChatActivity::class.java)
-                                        intent.putExtra("AI_RESPONSE", response)
-                                        startActivity(intent)
+                            // 첫 번째 질문 설정
+                            binding.tvQuestions.text = baseQuestions[currentQuestionIndex]
+                            // 힌트
+                            binding.tilName.hint = baseHints[currentQuestionIndex]
+
+                            // 처음에 "다음" 버튼 숨김
+                            binding.btnNext.visibility = View.GONE
+
+                            binding.etAnswers.addTextChangedListener(object : TextWatcher {
+                                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                                    //                TODO("Not yet implemented")
+                                }
+
+                                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                                    // 텍스트가 변경될 때 호출
+                                    binding.btnNext.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                                }
+
+                                override fun afterTextChanged(s: Editable?) {
+                                    //                TODO("Not yet implemented")
+                                }
+                            })
+
+                            binding.etAnswers.setText(u.username)
+
+                            binding.btnNext.setOnClickListener {
+                                val input = binding.etAnswers.text.toString()
+
+                                Log.d(TAG, "input :: $input")
+
+                                if (input.isEmpty()) {
+                                    Toast.makeText(this@MainActivity, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    // 반가워요! 부분을 안보이게 하고
+                                    binding.tvWelcome.visibility = View.INVISIBLE
+                                    // 입력된 데이터 저장
+                                    Constants.answers.add(input)
+
+                                    // 테이블에 저장
+                                    if (currentQuestionIndex == 0) {
+                                        u.nickname = input
+
+                                        GlobalScope.launch(Dispatchers.IO) {
+                                            val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
+                                            userDao.updateUser(u)
+                                        }
+                                    } else {
+
+                                        GlobalScope.launch(Dispatchers.IO) {
+                                            val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
+                                            val user = userDao.getUserByEmail(u.email)
+
+                                            val pet = Pet(
+                                                name = input,
+                                                userId = user?.id ?: 0L,
+                                                type = "",
+                                                breed = "",
+                                                age = 0,
+                                                bodyColor = "",
+                                                eyeColor = ""
+                                            )
+                                            val petDao = AppDatabase.getInstance(this@MainActivity).petDao()
+                                            petDao.insertPet(pet)
+                                        }
                                     }
-                                },
-                                onError = { error ->
-                                    runOnUiThread {
-                                        Toast.makeText(this@MainActivity, error, Toast.LENGTH_LONG).show()
-                                        Log.d("Error :", error)
+
+                                    // 입력란 초기화
+                                    binding.etAnswers.text!!.clear()
+
+                                    // 다음 질문으로 이동
+                                    if (currentQuestionIndex < baseQuestions.size - 1) {
+                                        currentQuestionIndex++
+                                        binding.tvQuestions.text = baseQuestions[currentQuestionIndex]
+                                        binding.tilName.hint = baseHints[currentQuestionIndex]
+                                    } else {
+                                        // 질문이 끝났을 경우
+                                        val intent = Intent(this@MainActivity, QuestionsActivity::class.java)
+
+                                        startActivity(intent)
+
+                                        finish()
                                     }
                                 }
-                            )
+                            }
+                        }
+                    } else {
+                        // 만약 둘 다 이미 정보가 있으면 바로 ChatActivity로 넘어감
+                        withContext(Dispatchers.Main) {
+
+                            user?.let { u ->
+                                pet?.let { p ->
+                                    val prompt = OpenAIClient().generatePrompt(u, p[0])
+
+                                    OpenAIClient().generateResponse(
+                                        prompt,
+                                        onSuccess = { response ->
+                                            runOnUiThread {
+                                                // 결과를 화면에 표시
+                                                val intent = Intent(this@MainActivity, ChatActivity::class.java)
+                                                intent.putExtra("AI_RESPONSE", response)
+                                                startActivity(intent)
+                                            }
+                                        },
+                                        onError = { error ->
+                                            runOnUiThread {
+                                                Toast.makeText(this@MainActivity, error, Toast.LENGTH_LONG).show()
+                                                Log.d("Error :", error)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
                         }
                     }
                 }
@@ -108,6 +204,9 @@ class MainActivity : AppCompatActivity() {
                         val userDao = AppDatabase.getInstance(this@MainActivity).userDao()
                         userDao.insertUser(userInfo)
                     }
+
+                    // 숨겼던 UI 복원
+                    binding.root.visibility = View.VISIBLE
 
                     // 첫 번째 질문 설정
                     binding.tvQuestions.text = baseQuestions[currentQuestionIndex]
